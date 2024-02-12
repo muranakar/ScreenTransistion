6be719490833cc07f45596db4110ab8246b49b8e
muranakar <P1211065@gmail.com>
2024-02-13 07:03:49 +0900

    add navigation修正

--- App.js
+++ App.js
@@ -1,20 +1,59 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Button, View, Text, StyleSheet } from 'react-native';
 
-export default function App() {
+
+const Tab = createBottomTabNavigator();
+
+function HomeTab({ navigation }) {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={[styles.container, { backgroundColor: 'white' }]}>
+      <Button
+        title="Go to Details"
+        onPress={() => {
+          navigation.navigate('Details');
+        }}
+      />
     </View>
+  );
+}
+
+function DetailsTab({ navigation }) {
+  return (
+    <View style={[styles.container, { backgroundColor: 'lightblue' }]}>
+      <Button
+        title="Go back to Home"
+        onPress={() => {
+          navigation.goBack();
+        }}
+      />
+    </View>
+  );
+}
+
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="Home" component={HomeTab} />
+        <Tab.Screen name="Details" component={DetailsTab} />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {
+    fontSize: 20,
+    marginBottom: 20,
   },
 });
+
+export default App;
